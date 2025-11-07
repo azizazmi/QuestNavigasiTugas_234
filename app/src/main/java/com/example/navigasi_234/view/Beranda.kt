84782b75443916b357cdbe1912be5e565c20565e
Azizah A. Azmi <azizah.aurellia.ft23@mail.umy.ac.id>
2025-11-07 15:57:54 +0700

    fear: add Scaffold layout dan painter resource

--- app/src/main/java/com/example/navigasi_234/view/Beranda.kt
+++ app/src/main/java/com/example/navigasi_234/view/Beranda.kt
@@ -1,8 +1,14 @@
 package com.example.navigasi_234.view
 
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.painterResource
+import androidx.compose.material3.R
 
 @Composable
 fun Beranda(OnSubmitBtnClick:()-> Unit) {
+    Scaffold { isiRuang->
+        val gambar = painterResource(R.drawable.getolookup)
 
+    }
 }
