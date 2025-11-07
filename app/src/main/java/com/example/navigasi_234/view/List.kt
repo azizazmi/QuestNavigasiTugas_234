09cdf12b1daeb46c07c7df4962d061e4ce946d13
Azizah A. Azmi <azizah.aurellia.ft23@mail.umy.ac.id>
2025-11-07 17:08:13 +0700

    feat: add function ListPeserta dan variable untuk menerima hasil data yang diinput atau data statis

--- app/src/main/java/com/example/navigasi_234/view/List.kt
+++ app/src/main/java/com/example/navigasi_234/view/List.kt
@@ -1,2 +1,19 @@
 package com.example.navigasi_234.view
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
+import com.example.navigasi_234.R
+
+@Composable
+fun ListPeserta(
+    onBackToBerandaClick:()-> Unit,
+    OnBtnToFormulirClick:()-> Unit
+) {
+    val daftar = listOf(
+        Pair(first = stringResource(R.string.namalengkap), second = "Danuarta"),
+        Pair(first = stringResource(R.string.jk), second = "Laki-laki"),
+        Pair(first = stringResource(R.string.status), second = "Lajang"),
+        Pair(first = stringResource(R.string.alamat), second = "Yogyakarta")
+    )
+
+}
