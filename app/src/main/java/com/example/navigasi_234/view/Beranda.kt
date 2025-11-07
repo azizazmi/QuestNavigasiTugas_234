7b3b7e69362d7d6491c5a709ba499d6000f320cf
Azizah A. Azmi <azizah.aurellia.ft23@mail.umy.ac.id>
2025-11-07 16:17:59 +0700

    refactor: memperbaiki import untuk R + feat: add text untuk selamat datang

--- app/src/main/java/com/example/navigasi_234/view/Beranda.kt
+++ app/src/main/java/com/example/navigasi_234/view/Beranda.kt
@@ -8,10 +8,12 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.res.painterResource
-import androidx.compose.material3.R
+import androidx.compose.material3.Text
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import com.example.navigasi_234.R
 
 @Composable
 fun Beranda(OnSubmitBtnClick:()-> Unit) {
@@ -24,7 +26,7 @@ fun Beranda(OnSubmitBtnClick:()-> Unit) {
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally)
         {
-
+            Text(text = stringResource(R.string.welcome))
         }
 
     }
