f4719e74c3fec3a1fd9c410457fbc3c5b48faa17
Azizah A. Azmi <azizah.aurellia.ft23@mail.umy.ac.id>
2025-11-07 16:14:32 +0700

    feat: add layout Column utama dan modifiernya

--- app/src/main/java/com/example/navigasi_234/view/Beranda.kt
+++ app/src/main/java/com/example/navigasi_234/view/Beranda.kt
@@ -1,14 +1,31 @@
 package com.example.navigasi_234.view
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.res.painterResource
 import androidx.compose.material3.R
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 
 @Composable
 fun Beranda(OnSubmitBtnClick:()-> Unit) {
     Scaffold { isiRuang->
         val gambar = painterResource(R.drawable.getolookup)
+        Column(modifier = Modifier
+            .fillMaxSize()
+            .background(color = Color(0xFFbddcf2))
+            .padding(paddingValues = isiRuang),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally)
+        {
+
+        }
 
     }
 }
